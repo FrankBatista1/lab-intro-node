9d2eeff2f276c8f0fbe4b6ab05dbd3e51528de2f
FrankBatista1 <fb53901@gmail.com>
2021-08-31 13:34:48 -0400

    Bonuses finished

--- index.js
+++ index.js
@@ -11,32 +11,42 @@ class SortedList {
   }
 
   get(pos) {
-    if(pos < this.length && pos >= 0){
-    return this.items[pos];
-    }else{
-      throw new Error('OutOfBounds');
+    if (pos < this.length && pos >= 0) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
     }
   }
 
   max() {
-    if (this.items[0]){
-      return this.items[this.length - 1]
-    } else{
-      throw new Error('EmptySortedList');
+    if (this.items[0]) {
+      return this.items[this.length - 1];
+    } else {
+      throw new Error("EmptySortedList");
     }
   }
 
   min() {
-    if (this.items[0]){
-      return this.items[0]
-    } else{
-      throw new Error('EmptySortedList');
+    if (this.items[0]) {
+      return this.items[0];
+    } else {
+      throw new Error("EmptySortedList");
     }
   }
 
-  sum() {}
+  sum() {
+    const sum = this.items.reduce((a, b) => a + b, 0);
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items[0]) {
+      const sum = this.items.reduce((a, b) => a + b, 0);
+      return sum / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
