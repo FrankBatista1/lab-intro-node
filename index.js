831553b7065d1bbe7e4899bae95d3e8ff9494eca
FrankBatista1 <fb53901@gmail.com>
2021-08-31 12:42:28 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos < this.length && pos >= 0){
+    return this.items[pos];
+    }else{
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items[0]){
+      return this.items[this.length - 1]
+    } else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items[0]){
+      return this.items[0]
+    } else{
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
